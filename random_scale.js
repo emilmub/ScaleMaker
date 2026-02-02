58a9637093173e06e7ff21ca0b46a22b6d3f337d
emilmub <emilmubarqui@gmail.com>
2026-02-02 20:47:50 +0100

    Add selection functionality

--- random_scale.js
+++ random_scale.js
@@ -4,26 +4,66 @@ const numberOfTones = 12;
 
 let scalesPromise = fetch("scales.json").then(r => r.json()) // Makes promise for reading scale data in json
 
-async function getRandomScale() {
+let scaleNames = [];
+let numberOfScales = 0;
+
+async function populateScales() {
     let scales = await scalesPromise;
 
-    let numberOfScales = scales["Scales"].length;
+    numberOfScales = scales["Scales"].length;
+
+    let name = "";
+    let scaleSelect = document.querySelector("#scale-select");
+    for (let scale of scales["Scales"]) {
+        name = scale["name"];
+        scaleNames.push(name);
+        let opt = document.createElement("option");
+        opt.value = name;
+        opt.innerText = name;
+        scaleSelect.appendChild(opt);
+    }
+}
+
+function selectScale() {
+
+    let scaleSelect = document.querySelector("#scale-select");
+    let toneSelect = document.querySelector("#tone-select");
+
+    let scaleIndex = scaleNames.indexOf(scaleSelect.value);
+    let toneIndex = tones.indexOf(toneSelect.value);
+
+    return [scaleIndex, toneIndex];
+}
+
+function getRandomScale() {
+
     let scaleIndex = Math.floor(Math.random() * numberOfScales);
-
-    let randomScale = scales["Scales"][scaleIndex]; // Chooses a scale
-
     let toneIndex = Math.floor(Math.random() * numberOfTones); // Chooses a tone for the scale
+
+    return [scaleIndex, toneIndex];
+}
+
+async function buildScale(scaleIndex, toneIndex) {
+    let scales = await scalesPromise;
+
+    let scale = scales["Scales"][scaleIndex];
+
     let h2 = document.querySelector('h2');
-    h2.innerText = `${tones[toneIndex]} ${randomScale["name"]}`
-    let scaleTones = [tones[toneIndex]];
+    h2.innerText = `${tones[toneIndex]} ${scale["name"]}`
+
     let scaleChords = [];
-    for (let index = 0; index < randomScale["tones"].length; ++index) {
-        scaleChords.push(randomScale["triads"][index]);
-        toneIndex += randomScale["tones"][index];
+    let scaleTones = [tones[toneIndex]];
+    for (let index = 0; index < scale["tones"].length; ++index) {
+        scaleChords.push(scale["triads"][index]);
+        toneIndex += scale["tones"][index];
         toneIndex %= numberOfTones;
-        scaleTones.push(tones[toneIndex]);
+        scaleTones.push(tones[toneIndex])
     }
 
+    return { scaleTones, scaleChords };
+}
+
+function showScale(scaleTones, scaleChords) {
     let toneDivs = document.querySelectorAll(".tone");
     let chordQualityDivs = document.querySelectorAll(".chord-quality");
     let accidentalsQualityDivs = document.querySelectorAll(".accidentals");
@@ -58,6 +98,22 @@ async function getRandomScale() {
     accidentalsQualityDivs[accidentalsQualityDivs.length - 1].innerText = accidentalsQualityDivs[0].innerText;
 }
 
-const randomize_btn = document.querySelector("#randomize");
+async function buttonRandomize() {
+    let [scaleIndex, toneIndex] = getRandomScale();
+    let scaleObject = await buildScale(scaleIndex, toneIndex);
+    showScale(scaleObject.scaleTones, scaleObject.scaleChords);
+}
 
-randomize_btn.onclick = getRandomScale;
+async function buttonSelection() {
+    let [scaleIndex, toneIndex] = selectScale();
+    let scaleObject = await buildScale(scaleIndex, toneIndex);
+    showScale(scaleObject.scaleTones, scaleObject.scaleChords);
+}
+
+populateScales();
+
+const select_btn = document.querySelector("#select-button");
+select_btn.onclick = buttonSelection;
+
+const randomize_btn = document.querySelector("#randomize");
+randomize_btn.onclick = buttonRandomize;
